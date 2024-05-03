5e8f28e31cab30e07607db548749bcce95a6ee0d
Rigon Pira <rp57421@ubt-uni.net>
2024-05-03 12:04:57 +0200

    Creating Service Interfaces

---
+++ Shortly.Data/Services/UrlsService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Services
+{
+	public class UrlsService : IUrlsService
+	{
+		private AppDbContext _context;
+
+		public UrlsService(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public List<Url> GetUrls()
+		{
+			var allUrls = _context.Urls.Include(n => n.User).ToList();
+			return allUrls;
+		}
+	}
+}
---
+++ Shortly.Data/Services/UsersService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortly.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortly.Data.Services
+{
+	public class UsersService : IUsersService
+	{
+		private AppDbContext _context;
+
+		public UsersService(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public List<User> GetUsers()
+		{
+			var users = _context.Users.Include(n => n.Urls).ToList();
+			return users;
+		}
+	}
+}
