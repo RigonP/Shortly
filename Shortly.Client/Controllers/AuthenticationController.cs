8c16cdc77c61c2e49695733636bd68e4da943c8d
Rigon Pira <rp57421@ubt-uni.net>
2024-05-06 23:41:32 +0200

    Logging Users Out Using SignInManager

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -79,5 +79,11 @@ namespace Shortly.Client.Controllers
 
 			return RedirectToAction("Index", "Home");
 		}
+
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index", "Home");
+		}
 	}
 }
